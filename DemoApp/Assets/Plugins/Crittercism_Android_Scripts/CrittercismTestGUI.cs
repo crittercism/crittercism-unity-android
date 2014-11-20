4e7a7926a53623e0c79989a2443160dec0c3cc75
David Shirley <dshirley@crittercism.com>
2014-11-20 13:09:00 -0800

    Architecture overhaul with substantially improved crash reporting
    Previously crash reports had java stack traces that did not pertain
    to crashes that occurred in C#.  This fixes that issue. It also
    simplifies the code.
    
    Created a new demo app from scratch.

---
+++ DemoApp/Assets/Plugins/Crittercism_Android_Scripts/CrittercismTestGUI.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class CrittercismTestGUI : MonoBehaviour
+{
+
+    void OnGUI ()
+    {
+        GUIStyle customStyle = new GUIStyle (GUI.skin.button);
+        customStyle.fontSize = 30;
+
+        int screenButtonHeight = Screen.height / 8;
+
+        if (GUI.Button (new Rect (0, 0, Screen.width, screenButtonHeight), "Leave breadcrumb", customStyle)) {
+            CrittercismAndroid.LeaveBreadcrumb ("BreadCrumb");
+        }
+
+        if (GUI.Button (new Rect (0, screenButtonHeight, Screen.width, screenButtonHeight), "Set User Metadata", customStyle)) {
+            CrittercismAndroid.SetUsername ("MommaCritter");
+            CrittercismAndroid.SetValue ("5", "Game Level");
+            CrittercismAndroid.SetValue ("Crashes a lot", "Status");
+        }
+
+        if (GUI.Button (new Rect (0, screenButtonHeight * 2, Screen.width, screenButtonHeight), "C# Crash", customStyle)) {
+            causeDivideByZeroException ();
+        }
+
+        if (GUI.Button (new Rect (0, screenButtonHeight * 3, Screen.width, screenButtonHeight), "C# Handled Exception", customStyle)) {
+            try {
+                causeDivideByZeroException ();
+            } catch (System.Exception e) {
+                CrittercismAndroid.LogHandledException (e);
+            }
+        }
+
+        if (GUI.Button (new Rect (0, screenButtonHeight * 4, Screen.width, screenButtonHeight), "C# Null Pointer Exception", customStyle)) {
+            causeNullPointerException ();
+        }
+    }
+
+    // Demo stacktraces by calling a few interim methods before crashing
+    void causeDivideByZeroException ()
+    {
+        interimMethod1 ("hi mom", 42);
+    }
+
+    void causeNullPointerException ()
+    {
+        object o = null;
+        o.GetHashCode ();
+    }
+
+    void interimMethod1 (string demoParam1, int demoParam2)
+    {
+        interimMethod2 (7, 7, "abc");
+    }
+
+    void interimMethod2 (byte demoParam1, int demoParam2, string demoParam3)
+    {
+        finallyDoTheCrash (100);
+    }
+
+    void finallyDoTheCrash (int number)
+    {
+		int[] numbers = { 1, 2, 3, 4, 5 };
+
+        numbers[10] /= 0;
+    }
+
+}
